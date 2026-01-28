2e4c177227c43898f2232f878a3985da895fee5a
Franck <amanifranck2005@gmail.com>
2026-01-28 10:41:21 +0100

    creating the project

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+console.log("Hello, TypeScript with NodeNext module system!");
+const name = "World";
+console.log(`Hello, ${name}!`);
+export {};
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+console.log("Hello, TypeScript with NodeNext module system!");
+
+const name : string = "World";
+console.log(`Hello, ${name}!`);
